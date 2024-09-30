08e6ede7f41ba99c1384a94f588049fea7276005
Jonesey86 <irj082024@gmail.com>
2024-09-30 11:12:16 +0200

    Json initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace SerializeBasic
+{
+    public class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string? Summary { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2024-09-24"),
+                TemperatureCelsius = 09,
+                Summary = "Cloudy"
+            };
+
+                string jsonString = JsonSerializer.Serialize(weatherForecast);
+
+            Console.WriteLine(jsonString);
+        }
+    }
+}
